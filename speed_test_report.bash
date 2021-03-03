811c7d281bacd0b3fd01c32d40cb7a7abdc3c337
Kyaw Sithu <kyawsithu@gtmh-telecom.com>
2021-03-03 16:30:19 +0630

    Update speed_test_report.bash

--- speed_test_report.bash
+++ speed_test_report.bash
@@ -16,4 +16,4 @@ do
 done
 
 curl  -u 'noc_bot:Gtmh123!@#' -X MKCOL  "https://app.gtmh-telecom.com/nextcloud/remote.php/dav/files/noc_bot/NOC_SPEEDTEST_REPORT/"$date""
-curl -T /usr/local/speedtest_daily_report/"$date"/speedtest_report_"$time".csv  -u 'noc_bot:Gtmh123!@#' "https://app.gtmh-telecom.com/nextcloud/remote.php/dav/files/noc_bot/NOC_SPEEDTEST_REPORT/"$date"/speedtest_report_"$time".csv"
+curl -T /home/speedtest_user/speedtest_daily_report/"$date"/speedtest_report_"$time".csv  -u 'noc_bot:Gtmh123!@#' "https://app.gtmh-telecom.com/nextcloud/remote.php/dav/files/noc_bot/NOC_SPEEDTEST_REPORT/"$date"/speedtest_report_"$time".csv"
