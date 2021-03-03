9d30bc9c62253790414b54c8ad65a63703595d32
Kyaw Sithu <kyawsithu@gtmh-telecom.com>
2021-03-03 16:12:16 +0630

    Update speed_test_report.bash

--- speed_test_report.bash
+++ speed_test_report.bash
@@ -3,15 +3,15 @@
 date=$(date +%F)
 time=$(date +%Ihr)
 
-mkdir --parents /usr/local/speedtest_daily_report/"$date"
+mkdir --parents /home/speedtest_user/speedtest_daily_report/"$date"
 
-speedtest -s 14062 --format=csv --output-header >> /usr/local/speedtest_daily_report/"$date"/speedtest_report_"$time".csv
+speedtest -s 14062 --format=csv --output-header >> /home/speedtest_user/speedtest_daily_report/"$date"/speedtest_report_"$time".csv
 
 for i in 31650 16916 30327 20670 20671 17590 17602 20882 
 
 do
 
-   speedtest -s $i --format=csv >> /usr/local/speedtest_daily_report/"$date"/speedtest_report_"$time".csv
+   speedtest -s $i --format=csv >> /home/speedtest_user/speedtest_daily_report/"$date"/speedtest_report_"$time".csv
 
 done
 
