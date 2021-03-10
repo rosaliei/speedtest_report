33cfbec0c2f87def2e6f97b805ff25602e9733eb
Kyaw Sithu <kyawsithu@gtmh-telecom.com>
2021-03-10 12:41:56 +0630

    Update human-readable format for speedtest

--- speed_test_report.bash
+++ speed_test_report.bash
@@ -5,13 +5,13 @@ time=$(date +%Ihr)
 
 mkdir --parents /home/speedtest_user/speedtest_daily_report/"$date"
 
-speedtest -s 14062 -u Mbps --format=csv --output-header >> /home/speedtest_user/speedtest_daily_report/"$date"/speedtest_report_"$time".csv
+speedtest -s 14062 -u Mbps >> /home/speedtest_user/speedtest_daily_report/"$date"/speedtest_report_"$time".csv
 
 for i in 31650 16916 30327 20670 20671 17590 17602 20882 
 
 do
 
-   speedtest -s $i -u Mbps --format=csv >> /home/speedtest_user/speedtest_daily_report/"$date"/speedtest_report_"$time".csv
+   speedtest -s $i -u Mbps >> /home/speedtest_user/speedtest_daily_report/"$date"/speedtest_report_"$time".csv
 
 done
 
